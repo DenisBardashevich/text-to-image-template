d5509a0b5dce58799cfabc238a827801557571cf
Denis <167361504+DenisBardashevich@users.noreply.github.com>
2025-09-04 13:55:20 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "дом у моря. Солнечная погода и бегают дети играют в мяч",
     };
 
     const response = await env.AI.run(
